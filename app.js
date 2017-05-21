534cba4aeda576793f8f519f352a1d38aaae446b
Ryo <kanadajinlee@yahoo.ca>
2017-05-21 01:17:15 -0500

    Testing the cron-job API with different alarms

--- app.js
+++ app.js
@@ -5,26 +5,56 @@ var CronJob = require('cron').CronJob;
 var app = express();
 
 
-/* The arguments in Cronjob:
+/* The arguments in "new Cronjob()":
 In order, here is what they represent (along with the allowed values).
   1) Seconds:     0–59
   2) Minutes:     0–59
   3) Hours:       0–23
   4) Day of Month:1–31
   5) Months:      0–11
-  6) Day of Week: 0–6        */
-new CronJob('* * * * * *', function() {
-    
-    console.log('A second has elapsed!!')
+  6) Day of Week: 0–6
+  note: the "*" as an argument means "each"        */
+//Seconds(0-59) Minutes(0-59) Hours(0-23) Day_Of_Month(1-31) Months(0-11) Day_Of_Week(0-6)
 
+//this works when using "require('cron').CronJob"
+new CronJob('* 30 * * * *', () => {
+    console.log('30 minutes have elapsed~~!!');
 }, null, true, 'America/Los_Angeles');
 
 
+// const example = new CronJob(‘00 00  06 *  * 1–5’, () => {
+//     console.log("00 seconds, 00 minutes,")
+//     console.log("at hour 6, on every day of the month,");
+//     console.log("from days 1 through 5 (so not running on Sat/Sundays).")
+// }, null, true, '');
+
+
+var job2 = new CronJob({
+  cronTime: '* * * * *',
+  onTick: function() {
+    console.log('job 2 ticked');
+  },
+  start: false,
+  timeZone: 'America/Los_Angeles'
+});
+
+// job2.start(); // job 2 started
+
+const everyMorning = new CronJob('00 15 08 * * 0-6', () => {
+  // This cronjob will run at 08:15:00(AM) everyday
+  console.log("BEEP BOOP, it's 8:15 mothafuckaa~");
+}, null, true, 'America/Chicago');
+
+const mornin = new CronJob('00 20 01 * * *', () => {
+  // This cronjob will run at 01:20:00(AM) everyday
+  console.log("BEEP BOOP, it's 1:05AM mothafuckaa~");
+}, null, true, 'America/Chicago');
+
 
 app.get("/", (req, res) => {
-  res.send("Good luck with CRON JOB")
+  res.send("Good luck with CRON JOB");
 })
 
-app.listen(3002, function () {
+app.listen(3002,  () => {
     console.log('Making some pancakes on port:', 3002);
 });
