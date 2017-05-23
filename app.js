9836f7dc85372710c2504b8ef32fd228ddc47e8c
Ryo <kanadajinlee@yahoo.ca>
2017-05-22 20:34:22 -0500

    Added cheerio, figuring out how it works

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 var express = require('express');
 var moment = require("moment");
 var CronJob = require('cron').CronJob;
-moment().format('MMMM Do YYYY, h:mm:ss a'); // May 22nd 2017, 5:38:04 pm
+var cheerio = require('cheerio'),
+var $ = cheerio.load('<ul id = "fruits">...</ul>');
 
 var app = express();
 
+/*
+  moment().fromNow(); //This will say "a few seconds ago"/"1 week ago", etc.
+*/
 
 /* The arguments in "new Cronjob()":
 In order, here is what they represent (along with the allowed values).
@@ -19,9 +23,12 @@ In order, here is what they represent (along with the allowed values).
 //Seconds(0-59) Minutes(0-59) Hours(0-23) Day_Of_Month(1-31) Months(0-11) Day_Of_Week(0-6)
 
 //this works when using "require('cron').CronJob"
-new CronJob('* 30 * * * *', () => {
-    console.log('30 minutes have elapsed~~!!');
-}, null, true, 'America/Los_Angeles');
+new CronJob('* * * * * *', () => {
+  // console.log('1 seconds have elapsed~~!!');
+  var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a'); // May 22nd 2017, 5:38:04 pm
+  console.log("This very moment is: ", rightNow)
+
+}, null, true, 'America/Chicago');
 
 
 // const example = new CronJob(‘00 00  06 *  * 1–5’, () => {
@@ -44,12 +51,13 @@ var job2 = new CronJob({
 
 const everyMorning = new CronJob('00 15 08 * * 0-6', () => {
   // This cronjob will run at 08:15:00(AM) everyday
-  console.log("BEEP BOOP, it's 8:15 mothafuckaa~");
+  console.log("BEEP BOOP, it's 8:15~");
+  console.log("This very moment is: ", rightNow)
 }, null, true, 'America/Chicago');
 
 const mornin = new CronJob('00 20 01 * * *', () => {
   // This cronjob will run at 01:20:00(AM) everyday
-  console.log("BEEP BOOP, it's 1:20AM mothafuckaa~");
+  console.log("BEEP BOOP, it's 1:20AM~");
 }, null, true, 'America/Chicago');
 
 
